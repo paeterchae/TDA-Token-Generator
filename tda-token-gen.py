37eb30416067517c970336b5408f3f9da1de196b
Peter Cha <pcha99@gmail.com>
2022-02-05 20:03:30 -0500

    first commit

---
+++ tda-token-gen.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from tda import auth
+import os
+from selenium import webdriver
+
+load_dotenv()
+
+token_path = 'token.json'
+api_key = os.getenv('API_KEY')
+redirect_url = 'https://localhost'
+
+with webdriver.Chrome() as driver:
+    c = auth.client_from_login_flow(
+        driver, api_key, redirect_url, token_path)
